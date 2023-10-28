b7ce022be78c3fe325cdd143d8a8d703ea799cc0
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-10-28 16:20:06 +0600

    refactor the home view

--- InstaFilter/HomeView.swift
+++ InstaFilter/HomeView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  HomeView.swift
 //  InstaFilter
 //
 //  Created by Shah Md Imran Hossain on 28/10/23.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    HomeView()
 }
