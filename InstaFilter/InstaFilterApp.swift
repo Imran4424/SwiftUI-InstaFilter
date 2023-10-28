4ca95c83abc7c5665b318dee6aa2d7e2f0eadf14
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-10-28 16:18:27 +0600

    insta filter

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Shah Md Imran Hossain on 28/10/23.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
