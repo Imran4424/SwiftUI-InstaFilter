413d29781bf6a5a8f48d80fd9edb7f043baa40e2
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-11-02 21:42:06 +0600

    confirm dialog for filter

--- InstaFilter/HomeView.swift
+++ InstaFilter/HomeView.swift
@@ -15,8 +15,10 @@ struct HomeView: View {
     @State private var filterIntensity = 0.5
     @State private var showingImagePicker = false
     
-    @State private var currentFilter = CIFilter.sepiaTone()
+    @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
     let context = CIContext()
+    
+    @State private var showingFilterSheet = false
     
     var body: some View {
         NavigationStack {
@@ -49,7 +51,7 @@ struct HomeView: View {
                 
                 HStack {
                     Button("Change Filter") {
-                        
+                        showingFilterSheet = true
                     }
                     
                     Spacer()
@@ -63,6 +65,17 @@ struct HomeView: View {
             .navigationTitle("InstaFilter")
             .sheet(isPresented: $showingImagePicker) {
                 ImagePicker(image: $inputImage)
+            }
+            .confirmationDialog("Select a filter", isPresented: $showingFilterSheet) {
+                Button("Crytallize") { setFilter(filter: CIFilter.crystallize()) }
+                Button("Edges") { setFilter(filter: CIFilter.edges()) }
+                Button("Crytallize") { setFilter(filter: CIFilter.crystallize()) }
+                Button("Gaussian Blur") { setFilter(filter: CIFilter.gaussianBlur()) }
+                Button("Pixellate") { setFilter(filter: CIFilter.pixellate()) }
+                Button("Sepia Tone") { setFilter(filter: CIFilter.sepiaTone()) }
+                Button("Unsharp Mask") { setFilter(filter: CIFilter.unsharpMask()) }
+                Button("Vignette") { setFilter(filter: CIFilter.vignette()) }
+                Button("Cancel", role: .cancel) { }
             }
             .onChange(of: inputImage) {
                 loadImage()
@@ -84,8 +97,25 @@ extension HomeView {
         applyProcessing()
     }
     
+    func setFilter(filter: CIFilter) {
+        currentFilter = filter
+        loadImage()
+    }
+    
     func applyProcessing() {
-        currentFilter.intensity = Float(filterIntensity)
+        let inputKeys = currentFilter.inputKeys
+        
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey)
+        }
+        
+        if inputKeys.contains(kCIInputRadiusKey) {
+            currentFilter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey)
+        }
+        
+        if inputKeys.contains(kCIInputScaleKey) {
+            currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey)
+        }
         
         guard let outputImage = currentFilter.outputImage else {
             return
